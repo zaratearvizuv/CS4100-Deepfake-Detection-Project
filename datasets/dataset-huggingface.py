2f810600f1976dd4a04a7388c1f4bd796082b093
Shiyao Chen <chen.shiyao@northeastern.edu>
2026-03-18 02:43:28 -0400

    change dataset using example codes

--- datasets/dataset-huggingface.py
+++ datasets/dataset-huggingface.py
@@ -1,11 +1,9 @@
 import datasets
-from torch.utils.data import DataLoader
 
-# Load dataset
-ossia = datasets.load_dataset("ductai199x/open-set-synth-img-attribution", "arch")
+ossia = datasets.load_dataset("ductai199x/open-set-synth-img-attribution", "arch") # or "gen"
 
-# Use it
-dataloader = DataLoader(ossia["train"], batch_size=32, shuffle=True)
-for batch in dataloader:
-    # training loop ...
-    pass
+# see structure
+print(ossia)
+
+# access the first example
+print(ossia["train"][0])
